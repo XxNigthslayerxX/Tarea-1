12f3aa8ef4e737d83337c010c73d4d695ce326ae
Akira <Akira@DESKTOP-EOKMI52>
2024-09-13 23:29:34 -0600

    Agregar archivos de proyecto.

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,29 @@
+using Test1.Client.Pages;
+using Test1.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Test1.Client._Imports).Assembly);
+
+app.Run();
